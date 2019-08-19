f3c058299e88481766440a75dcefe97f06efae8c
rassy yassine <yassine.rassy1@gmail.com>
2019-08-19 13:40:21 +0200

    adapt app module to the navBar branch

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,24 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { BlocksComponent } from './Components/blocks/blocks.component';
-import { CarouselComponent } from '../UiComponents/carousel/carousel.component';
-import { FillPipe } from './Components/comment/fill.pipe';
-import { CarouselItemDirective } from '../UiComponents/carousel/carousel-item.directive';
-import { CarouselElementDirective } from '../UiComponents/carousel/carousel-element.directive';
-import { CommentComponent } from './Components/comment/comment.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CommentComponent,
-    CarouselComponent, 
-    FillPipe, 
-    CarouselComponent, 
-    CarouselItemDirective,
-    CarouselElementDirective,
-    BlocksComponent,
     NavBarComponent
   ],
   imports: [

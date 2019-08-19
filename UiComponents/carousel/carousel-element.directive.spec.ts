c107e16752876ac88343298a5054b6224df8dc07
rassy yassine <yassine.rassy1@gmail.com>
2019-08-19 13:36:03 +0200

    delete files no related to navBar component

--- UiComponents/carousel/carousel-element.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CarouselElementDirective } from './carousel-element.directive';
-
-describe('CarouselElementDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CarouselElementDirective();
-    expect(directive).toBeTruthy();
-  });
-});
